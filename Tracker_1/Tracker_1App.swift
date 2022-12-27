33e20d198ad8fd36363e5e2864c5f589b62e6232
BubblyDrink <hero68o@yahoo.com>
2022-12-26 21:32:28 -0600

    Added Chart to view data and tab bar.

---
+++ Tracker_1/Tracker_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Tracker_1App.swift
+//  Tracker_1
+//
+//  Created by Eric Martinez on 12/19/22.
+//
+
+import SwiftUI
+
+@main
+struct Tracker_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
