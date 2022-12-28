86d4a6cce9d61cbc5ab2c6d4c936166ba6178e14
BubblyDrink <hero68o@yahoo.com>
2022-12-27 23:12:19 -0600

    Added custom tab bar with water button

--- Tracker_1/ContentView.swift
+++ Tracker_1/ContentView.swift
@@ -7,96 +7,130 @@
 import Charts
 import SwiftUI
 
+enum Tab {
+    case first
+    case second
+}
+
+struct TabButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .opacity(configuration.isPressed ? 0.9 : 1)
+            .scaleEffect(configuration.isPressed ? 0.98 : 1)
+    }
+}
+
+
+struct ContentView: View {
+    @State private var selectedTab: Tab = .first
+    var body: some View {
+        VStack{
+            switch selectedTab {
+            case.first:
+                NavigationView {
+                    HomeView()
+                }
+            case .second:
+                NavigationView {
+                    SettingsView()
+                }
+            }
+            CustomTabView(selectedTab: $selectedTab)
+                .frame(height: 50)
+        }
+    }
+}
+
+struct CustomTabView: View {
+    @Binding var selectedTab: Tab
+    var body: some View {
+        HStack {
+            Spacer()
+            Button {
+                selectedTab = .first
+            } label: {
+                VStack {
+                    Image(systemName: "calendar")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(.primary)
+                }
+                .foregroundColor(selectedTab == .first ? .blue : .primary)
+            }
+            Spacer()
+            Button {
+        
+            } label: {
+                ZStack{
+                    Circle()
+                        .foregroundColor(.white)
+                        .frame(width: 80, height: 80)
+                        .shadow(radius: 2)
+                    Image(systemName: "drop.circle.fill")
+                        .resizable()
+                        .foregroundColor(.blue)
+                        .frame(width: 72, height: 72)
+                }
+                .offset(y: -32)
+            }
+            .buttonStyle(TabButtonStyle())
+            Spacer()
+            Button {
+                selectedTab = .second
+            } label: {
+                VStack {
+                    Image(systemName: "gearshape.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(.primary)
+                }
+                .foregroundColor(selectedTab == .second ? .blue : .primary)
+            }
+            Spacer()
+        }
+    }
+}
+
 struct Item: Identifiable {
     var id = UUID()
     let type: String
     let value: Double
 }
 
-struct ContentView: View {
-    var body: some View {
-        TabView() {
-            HomeView()
-                .tabItem {
-                    Image(systemName: "house")
-                }
-            SettingsView()
-                .tabItem {
-                    Image(systemName: "gear")
-                }
-            CalendarView()
-                .tabItem {
-                    Image(systemName: "calendar")
-                }
+    struct HomeView: View{
+        let items: [Item] = [
+            Item(type:"M", value: 100),
+            Item(type:"T", value: 80),
+            Item(type:"W", value: 70),
+            Item(type:"T", value: 30),
+            Item(type:"F", value: 76),
+            Item(type:"S", value: 90),
+            Item(type:"S", value: 58),
+        ]
+        
+        var body: some View{
+            Chart(items) { item in
+                BarMark(
+                    x: .value("Days", item.type),
+                    y: .value("Water", item.value))
+            }
+            .navigationTitle("First")
+            .frame(height: 200)
         }
     }
-}
-
-struct CalendarView: View {
-    var body: some View {
-        NavigationView {
-            ZStack {
-                Color.green
-            }
-            .navigationTitle("Month at a glance")
+    
+    struct SettingsView: View{
+        var body: some View {
+            Color(.systemGray6)
+                .ignoresSafeArea()
+                .navigationTitle("Second")
         }
     }
-}
-
-struct SettingsView: View{
-    var body: some View{
-        NavigationView{
-            ZStack{
-                Color.blue
-            }
-            .navigationTitle("Settings")
-        }
-    }
-}
-
-
-struct HomeView: View {
-    // Put info/variables under struct
-    let items: [Item] = [
-        Item(type:"M", value: 100),
-        Item(type:"T", value: 95),
-        Item(type:"W", value: 80),
-        Item(type:"Th", value: 35),
-        Item(type:"F", value: 70),
-        Item(type:"Sa", value: 40),
-        Item(type:"Su", value: 60)
-    ]
     
-    var body: some View{
-        NavigationView{
-            ZStack{
-                Chart(items) { item in
-                    BarMark(
-                        x: .value("Day", item.type),
-                        y: .value("Water", item.value))
-                    .cornerRadius(5)
-                    
-                }
-                .foregroundStyle(Color.blue.gradient)
-                .frame(height:200)
-                .padding()
-            }
-        .navigationTitle("Week at a glance")
-    }
-}
-
-
-    
-
-    
-
-            
-            
-            
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-        }
     }
 }
-
